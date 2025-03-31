bffce486510afe0d9224ad4231ac035fc704fffa
Shreya <shreyavkd@gmail.com>
2025-03-31 04:13:39 +0100

    Updated simulation header

--- monte_carlo_sim.h
+++ monte_carlo_sim.h
@@ -1,10 +1,6 @@
 #ifndef MONTE_CARLO_SIM_H
 #define MONTE_CARLO_SIM_H
 
-#include <iostream>
-#include <vector>
-#include <random>
-#include <math.h>
 #include "system.h"
 
 using namespace std;
@@ -12,19 +8,16 @@ using namespace std;
 class Monte_Carlo_Sim
 {
     private:
-        System system;
-        int J = 1;
+        System &system;
         double beta;
 
-        std::vector<int> energies;
-        std::vector<int> magnetisations;
+        // std::vector<int> energies;
+        // std::vector<int> magnetisations;
 
     public:
-        Monte_Carlo_Sim(System s, double b);
+        Monte_Carlo_Sim(System &s, double b);
         void monte_carlo_step(int energy_change, int rand_choice_index);
         void run_simulation(int num_iterations);
-        void record_data(int energy, int magnetisation);
-        void save(const std::string &filename);
 };
 
 #endif
--- system.h
+++ system.h
@@ -1,26 +1,23 @@
 #ifndef SYSTEM_H
 #define SYSTEM_H
 
-#include <iostream>
 #include <vector>
 #include <random>
 
 class System 
 {
     private:
-        double num_population;
+        int num_population;
         double J;
         std::vector<int> sites;
 
     public:
-        System();
-        System(int N); //default constructor - 1 dim
-        System(int N, int M); //overloaded constructor - 2 dimensions
+        System(int N, double J=1); //constructor - 1 dim
 
         int get_spin(int index);
         void set_spin(int index, int spin);
 
-        double population_size(); //returns num_population
+        int population_size(); //returns num_population
 
         double energy(); 
         double magnetisation();
